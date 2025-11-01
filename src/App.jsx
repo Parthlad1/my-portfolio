ae81fa719603bc2353d66f559a1ac186c1d35026
Parth Lad <parthlad153@gmail.com>
2025-11-01 23:26:22 +0530

    Added Home Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar/Navbar.jsx'
+import Home from './sections/Home.jsx'
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <Home />
     </>
   )
 }
---
+++ src/sections/Home.jsx
@@ -0,0 +1,29 @@
+import './Home.css'
+
+function Home() {
+  return (
+    <section id="home">
+        <img className="home-img" src="https://img.pikbest.com/origin/10/39/14/59WpIkbEsTVxf.jpg!w700wp" alt="Profile"/>
+        <div className="home-content">
+            <h1>Hi, I'm <span>Parth Lad</span></h1>
+            <h2>Full Stack Developer | Automation Enthusiast | Tech Explorer</h2>
+            <p>
+            I create scalable, data-driven, and user-friendly web applications using React.js, Java, Python, and SQL.
+            Passionate about blending frontend creativity with backend precision, I love building tools that
+            simplify work and make technology more intuitive.
+            </p>
+            <div className="home-buttons">
+                <a href="#projects" className="home-btn">View My Work</a>
+                <a href="/ParthLad_Resume.pdf" target="_blank" rel="noopener noreferrer" className="home-btn">
+                    View Resume</a>
+                <a href="#contact" className="home-btn">Contact Me</a>
+                <a href="https://github.com/Parthlad1" target="_blank" rel="noopener noreferrer" className="home-btn">
+                GitHub</a>
+            </div>
+        </div>
+    </section>
+
+  );
+}
+
+export default Home;

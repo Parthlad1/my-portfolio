c2cb7dd86bde25ff2731515f24a35ba619000afb
Parth Lad <parthlad153@gmail.com>
2025-11-02 15:54:17 +0530

    Added Hamburger Menu to Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,34 @@
+import { useState } from 'react';
 import './Navbar.css';
+import { GiHamburgerMenu } from "react-icons/gi";
 
 function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  }
+
+  const closeMenu = () => {
+    setShowMenu(false);
+  }
+
   return (
     <nav className="navbar">
       <div className="logo">Parth Lad</div>
-      <ul className="nav-links">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#skills">Skills</a></li>
-        <li><a href="#experience">Experience</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#contact">Contact</a></li>
+      <ul className={showMenu ? "nav-link-mobile" : "nav-links"}>
+        <li><a href="#home" onClick={closeMenu}>Home</a></li>
+        <li><a href="#about" onClick={closeMenu}>About</a></li>
+        <li><a href="#skills" onClick={closeMenu}>Skills</a></li>
+        <li><a href="#experience" onClick={closeMenu}>Experience</a></li>
+        <li><a href="#projects" onClick={closeMenu}>Projects</a></li>
+        <li><a href="#contact" onClick={closeMenu}>Contact</a></li>
       </ul>
+      <div className='ham-menu'>
+        <button onClick={toggleMenu}>
+          <GiHamburgerMenu/>
+        </button>
+      </div>
     </nav>
   );
 }
